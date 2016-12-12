be8cefccce93f6f63da691f4ea05707df0ee616d
Ricardo Rover <ricardo@joaorover.com>
2016-12-11 22:25:23 -0200

    Avoid music to have more than once instances
    - This can happen when user taps back button on Nickname screen.

--- game/classes/ui/welcome.js
+++ game/classes/ui/welcome.js
@@ -6,18 +6,19 @@ function Welcome(){
 	// Load all the needed resources for the menu.
 	game.load.image('welcome', 'game/assets/welcome_splash.png');
     game.load.audio('menuMusic', ['game/assets/sound/menu_music.mp3']);
-    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;    
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.scale.pageAlignHorizontally = true;
     this.scale.updateLayout();
 };
 
 Welcome.prototype.create = function () {
 	this.add.button(0, 0, 'welcome', this.startNickName, this);
-    this.musicPlayer = game.add.audio('menuMusic', 0.2, false);
-    this.musicPlayer.play('', 0, 0.2, false);
+    if (!this.musicPlayer) {
+        this.musicPlayer = game.add.audio('menuMusic', 0.2, false);
+        this.musicPlayer.play('', 0, 0.2, false);
+    }
 };
 
 Welcome.prototype.startNickName = function () {
 	this.state.start('Nickname');
 };
-
